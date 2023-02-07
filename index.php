37a999a193832fec303851d5deb3d09f592a3713
Adam Wise <wise.adam@student.greenriver.edu>
2023-02-07 05:15:38 -0500

    added a new view for the page user will be logged into. doesnt really work. issue with submitting form, wont take me to the specifie page, cant figure it out

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //This is my controller
-
+session_start();
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -20,12 +20,37 @@ $f3->route('GET /', function(){
 
 });
 
+//Define a home route
+$f3->route('GET /home', function(){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/home-page.html");
+
+});
+
 //Define a login route
-$f3->route('GET /login', function(){
+$f3->route('GET|POST /login', function($f3){
+    var_dump($_POST);
+    if($_SERVER['REQUEST_METHOD'] == ['POST']){
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
+
+        //redirect to logged in frontpage(but home for right now)
+        $f3->reroute('views/frontpage');
+    }
 
     //Instantiate a view
     $view = new Template();
     echo $view->render("views/login.html");
+
+});
+
+$f3->route('GET /frontpage', function(){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/front-page.html");
 
 });
 
