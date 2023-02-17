bacbd0dcc8c53dbe048ed6a3d43bc1e98d9f0d7c
Tom Loudon <tloudong@srv603.webhostingforstudents.com>
2023-02-16 23:55:13 -0500

    added number labels to cooking instruction inputs

--- javascript/createRecipeDynamic.js
+++ javascript/createRecipeDynamic.js
@@ -1,12 +1,20 @@
+
 function addInput() {
     var inputs = document.querySelectorAll('input[type="text"]');
     var lastInput = inputs[inputs.length - 1];
     if (lastInput.value !== '') {
+
+        var newSpan = document.createElement('span'); // create a new span element to hold the input number
+        var inputCounter = inputs.length - 2;
+        newSpan.textContent = inputCounter + '. ';
+
+
         var newInput = document.createElement('input');
         newInput.type = 'text';
         newInput.name = 'input' + (inputs.length + 1);
         newInput.className = 'w-100';
         newInput.addEventListener('input', addInput);
+
         var newDivRow = document.createElement('div'); // create a new div element with class 'row'
         newDivRow.className = 'row mt-2';
 
@@ -15,6 +23,7 @@ function addInput() {
 
         var newDivCol8 = document.createElement('div'); // create a new div element with class 'col-8'
         newDivCol8.className = 'col-8';
+        newDivCol8.appendChild(newSpan);
         newDivCol8.appendChild(newInput); // add the new input element to the new col-8 div element
 
         newDivRow.appendChild(newDivCol2); // add the new col-2 div element to the new row div element
